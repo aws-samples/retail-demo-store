d0293f28f358e7b0ce5e6a2705382f6324c35f21
James Jory <james@jamesjory.com>
2020-08-07 15:48:35 -0700

    Filters cleanup, lodash, load all users (#53)
    * Increase user count to account for larger dataset
    
    * Add filters to Personalize cleanup logic
    
    * Ignore model files for git
    
    * Upgrade lodash dependency

--- src/aws-lambda/personalize-delete-resources/bundle.sh
+++ src/aws-lambda/personalize-delete-resources/bundle.sh
@@ -6,6 +6,7 @@ PACKAGE_FILE=personalize-delete-resources.zip
 
 echo "Cleaning up intermediate files"
 [ -e ${PACKAGE_FILE} ] && rm ${PACKAGE_FILE}
+[ -e "models" ] && rm -rf models
 [ -e "package" ] && rm -rf package
 
 echo "Installing Lambda dependencies"
@@ -16,7 +17,12 @@ cd package
 zip -r9 ${OLDPWD}/${PACKAGE_FILE} .
 cd ${OLDPWD}
 
+echo "Downloading Personalize SDK model files"
+wget -q https://raw.githubusercontent.com/boto/botocore/e22aad94ba9f33bda7a0abea3a959fb1af56b25d/botocore/data/personalize-runtime/2018-05-22/service-2.json -P ./models/personalize-runtime/2018-05-22
+wget -q https://raw.githubusercontent.com/boto/botocore/e22aad94ba9f33bda7a0abea3a959fb1af56b25d/botocore/data/personalize/2018-05-22/service-2.json -P ./models/personalize/2018-05-22
+
 echo "Adding Lambda function source code to package"
 zip -g ${PACKAGE_FILE} ${LAMBDA_SOURCE}
+zip -gr ${PACKAGE_FILE} models
 
 echo "Done!"
