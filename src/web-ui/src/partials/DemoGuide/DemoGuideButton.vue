8cb2cc466c0274d815b4eefa7c6defea51ec2c10
Kaz Baig <kazbaig@amazon.com>
2020-11-11 11:31:00 -0600

    Sticky Demo Guide button

---
+++ src/web-ui/src/partials/DemoGuide/DemoGuideButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="button btn btn-primary">
+    <div class="demo-guide">DEMO GUIDE</div>
+    <div class="learn-more">Learn more about this demo</div>
+  </button>
+</template>
+
+<script>
+export default { name: 'DemoGuideButton' };
+</script>
+
+<style scoped>
+  .button {
+    border: none;
+    border-radius: 12px 12px 0 0;
+    background: var(--blue-500);
+  }
+
+  .button:hover, .button:focus {
+    background: var(--blue-600);
+  }
+
+  .demo-guide {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+
+  .learn-more {
+    font-weight: 500;
+    font-size: 1.35rem;
+  }
+</style>
