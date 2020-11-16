2b23e64c9b34d513ac3ce1209c7c803ece04abcb
Kaz Baig <kazbaig@amazon.com>
2020-11-16 16:14:07 -0600

    remove copy that is only sometimes relevant

--- src/web-ui/src/public/Welcome.vue
+++ src/web-ui/src/public/Welcome.vue
@@ -1,60 +1,40 @@
 <template>
   <Layout :showNav="false" :showFooter="false" backgroundColor="var(--aws-squid-ink)">
     <div class="container mb-2 text-left">
-      <header class="px-3 px-md-5">
-        <h1 class="heading my-5 text-center">Welcome to the Retail Demo Store!</h1>
+      <h1 class="heading my-5 text-center">Welcome to the Retail Demo Store!</h1>
 
-        <p>
-          The Retail Demo Store is a demo platform to demonstrate how AWS infrastructure and services can be used to
-          build personalized customer experiences across multiple channels of engagement.
-        </p>
+      <p>
+        The Retail Demo Store is a demo platform to demonstrate how AWS infrastructure and services can be used to build
+        personalized customer experiences across multiple channels of engagement.
+      </p>
 
-        <p>
-          The Retail Demo Store mimics an online store with products, users and user-interactions to showcase the
-          personalized product recommendations a shopper would receive based on historical and real time click-through
-          behavior.
-        </p>
+      <p>
+        The Retail Demo Store mimics an online store with products, users and user-interactions to showcase the
+        personalized product recommendations a shopper would receive based on historical and real time click-through
+        behavior.
+      </p>
 
-        <div class="mt-2 d-flex flex-column flex-lg-row align-items-lg-center">
-          <div class="mb-2 mb-lg-0">AWS Services Included:</div>
+      <div class="mt-2 d-flex flex-column flex-lg-row align-items-lg-center">
+        <div class="mb-2 mb-lg-0">AWS Services Included:</div>
 
-          <ul
-            class="service-list mb-0 pl-0 flex-grow-1 d-flex flex-column flex-lg-row justify-content-lg-around font-weight-bold"
-          >
-            <li v-for="service in services" :key="service.text" class="mb-2 mb-lg-0">
-              <ServiceBadge :text="service.text" :src="service.src"></ServiceBadge>
-            </li>
-          </ul>
+        <ul
+          class="service-list mb-0 pl-0 flex-grow-1 d-flex flex-column flex-lg-row justify-content-lg-around font-weight-bold"
+        >
+          <li v-for="service in services" :key="service.text" class="mb-2 mb-lg-0">
+            <ServiceBadge :text="service.text" :src="service.src"></ServiceBadge>
+          </li>
+        </ul>
+      </div>
+
+      <!-- make both links to go /auth until we have dedicated sign in and create account pages -->
+      <div class="auth-cta my-5 d-flex flex-column flex-sm-row justify-content-end align-items-center">
+        <div class="login-cta d-flex align-items-center">
+          Have an account?<router-link to="/auth" class="sign-in ml-3 btn btn-link">Sign in</router-link>
         </div>
-
-        <!-- make both links to go /auth until we have dedicated sign in and create account pages -->
-        <div class="auth-cta my-5 d-flex flex-column flex-sm-row justify-content-end align-items-center">
-          <div class="login-cta d-flex align-items-center">
-            *Have an account?<router-link to="/auth" class="sign-in ml-3 btn btn-link">Sign in</router-link>
-          </div>
-          <router-link to="/auth" class="create-account mt-3 mt-sm-0 ml-sm-3 btn btn-primary">Create an account</router-link>
-        </div>
-      </header>
-
-      <section class="info py-3 px-3 py-md-4 px-md-5">
-        <p>
-          *To keep the click-through data fresh, this demo resets every day at 11 pm EST. Accounts created more than 24
-          hours prior would be deleted as well. If your account has been deleted, go ahead and create a new one.
-        </p>
-        <p class="mb-0">
-          This project also comes with a collection of workshops that are designed to walk a technical audience, either
-          in a group setting or self-paced as an individual, through adding enhanced functionality to the Retail Demo
-          Store. There are workshops for adding search, personalization, experimentation framework, user messaging, and
-          more. For an overview of the Retail Demo Store, its architecture, and workshops, please see the
-          <a
-            href="https://github.com/aws-samples/retail-demo-store/tree/master/workshop"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="workshop-link"
-            >workshop welcome notebook page</a
-          >.
-        </p>
-      </section>
+        <router-link to="/auth" class="create-account mt-3 mt-sm-0 ml-sm-3 btn btn-primary"
+          >Create an account</router-link
+        >
+      </div>
     </div>
   </Layout>
 </template>
