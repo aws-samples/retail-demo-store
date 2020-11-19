37b7907a4d617a2253aa7b5c5a9d97093a176f33
Egor Mesyats <egor.mesyats@moovweb.com>
2020-11-19 12:57:52 +0200

    forcePrefetchRatio: 0

--- src/web-ui/xdn/browser.js
+++ src/web-ui/xdn/browser.js
@@ -4,7 +4,7 @@ import install from "@xdn/prefetch/window/install";
 document.addEventListener("DOMContentLoaded", () => {
   console.info("[XDN browser] DOMContentLoaded -> running install()");
   install({
-    forcePrefetchRatio: 0.5, // forcely prefetch 50% of non-cached content for higher hit rate
+    forcePrefetchRatio: 0,
   });
   console.info("[XDN browser] DOMContentLoaded -> running installDevtools()");
   installDevtools();
