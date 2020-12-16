00b73bae02a3740155dee15bc9182c08dd02c2ad
Kaz Baig <kazbaig@amazon.com>
2020-12-15 18:21:08 -0600

    use consistent close icon

--- src/web-ui/src/partials/ConfirmationModal/ConfirmationModalLayout/ConfirmationModalLayout.vue
+++ src/web-ui/src/partials/ConfirmationModal/ConfirmationModalLayout/ConfirmationModalLayout.vue
@@ -4,7 +4,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+            <i class="fa fa-times"></i>
           </button>
         </div>
 
