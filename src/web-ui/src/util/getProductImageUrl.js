cdbe3911f0c0687d451c3e3973c88b0d53ea9b4e
James Jory <james@jamesjory.com>
2021-01-27 18:18:35 -0800

    IVS integration fixes (#202)
    * Add IVS products back; fix mappings
    
    * Default product image
    
    * Remove catalog images from repo
    
    * Adjust categories and styles; move id to top

--- src/web-ui/src/util/getProductImageUrl.js
+++ src/web-ui/src/util/getProductImageUrl.js
@@ -1,4 +1,6 @@
 export const getProductImageUrl = (product) => {
+  if (!product.image) return `${process.env.VUE_APP_IMAGE_ROOT_URL}/product_image_coming_soon.png`;
+
   if (product.image.includes('://')) return product.image;
 
   return `${process.env.VUE_APP_IMAGE_ROOT_URL}${product.category}/${product.image}`;
