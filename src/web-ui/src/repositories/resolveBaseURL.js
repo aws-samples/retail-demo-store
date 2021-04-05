d564c3e64d63a8a38440c2bf6496517f8663c62b
Egor Mesyats <egor.mesyats@moovweb.com>
2021-04-05 15:37:59 +0300

    fix resolveBaseURL port bug

--- src/web-ui/src/repositories/resolveBaseURL.js
+++ src/web-ui/src/repositories/resolveBaseURL.js
@@ -1,7 +1,7 @@
 function resolveBaseURL(serviceDomain, servicePort, servicePath) {
   servicePort = servicePort || ''
   servicePath = servicePath || '/'
-  return `${serviceDomain}${servicePort}${servicePath}`
+  return `${serviceDomain}${servicePort ? `:${servicePort}` : ''}${servicePath}`
 }
 
 export default resolveBaseURL
