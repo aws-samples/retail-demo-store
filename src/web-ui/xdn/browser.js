0fb76b3dadcd72db983adc0a6a4e91910f3afb91
Egor Mesyats <egor.mesyats@moovweb.com>
2021-04-05 13:36:13 +0300

    disable xdn devtools

--- src/web-ui/xdn/browser.js
+++ src/web-ui/xdn/browser.js
@@ -1,11 +1,9 @@
-import installDevtools from "@xdn/devtools/install";
+// import installDevtools from "@xdn/devtools/install";
 import install from "@xdn/prefetch/window/install";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.info("[XDN browser] DOMContentLoaded -> running install()");
-  install({
-    forcePrefetchRatio: 0,
-  });
-  console.info("[XDN browser] DOMContentLoaded -> running installDevtools()");
-  installDevtools();
+  install();
+  // console.info("[XDN browser] DOMContentLoaded -> running installDevtools()");
+  // installDevtools();
 });
