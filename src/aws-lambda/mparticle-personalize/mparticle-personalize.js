00640f10fe71e2e0126469c3c0e21461639837a9
Igor <1186878+manbearshark@users.noreply.github.com>
2021-10-25 10:44:49 -0700

    Added license statement to mp js code

--- src/aws-lambda/mparticle-personalize/mparticle-personalize.js
+++ src/aws-lambda/mparticle-personalize/mparticle-personalize.js
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 const AWS = require('aws-sdk');
 const JSONBig = require('json-bigint')({ storeAsString: true });
 const mParticle = require('mparticle');
