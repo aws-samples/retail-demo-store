ba821f3c2b3d5546a63afc2560cb98233808bfbc
Jeff Patzer <jeff.patzer@layer0.co>
2021-12-06 13:10:09 -0700

    add precache call to service worker

--- src/web-ui/layer0/service-worker.js
+++ src/web-ui/layer0/service-worker.js
@@ -3,8 +3,10 @@
 
 import { Prefetcher } from "@layer0/prefetch/sw";
 import { clientsClaim, skipWaiting } from "workbox-core";
+import { precacheAndRoute } from "workbox-precaching";
 
 skipWaiting();
 clientsClaim();
+precacheAndRoute(self.__WB_MANIFEST || []);
 
 new Prefetcher().route();
