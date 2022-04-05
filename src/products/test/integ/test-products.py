e2965e7ae8ea4c0d401223bafc8ee9281629ba04
Daisy Riley <rileydai@amazon.co.uk>
2022-04-05 16:32:12 +0100

    Modify products service tests to use test helper library instead of own methods

--- src/products/test/integ/test-products.py
+++ src/products/test/integ/test-products.py
@@ -1,142 +1,76 @@
-import jsonschema
-import requests
-import json
+import testhelpers.integ as integhelpers
 import os
 
-
+cwd = os.path.dirname(os.path.abspath(__file__))
+request_bodies_path = integhelpers.absolute_file_path(cwd, "json_request_bodies.json")
+schemas_path = integhelpers.absolute_file_path(cwd, "json_schemas.json")
 products_api_url = os.getenv('PRODUCTS_API_URL')
 cwd = os.path.dirname(os.path.abspath(__file__))
-wildcards = {
-    ":product_id": os.getenv('TEST_PRODUCT_ID'),
-    ":category_name": os.getenv('TEST_CATEGORY_ID'),
-    ":category_id": os.getenv('TEST_CATEGORY_ID')
-}
-
-
-def full_request_url(text):
-    """
-    Build a full request URL from the API URL and endpoint.
-    Any URL parameters will be replaced with the value set in the environment variables.
-    """
-    for key in wildcards.keys():
-        text = text.replace(key, str(wildcards[key]))
-    return products_api_url + text
-
-
-def absolute_file_path(filename):
-    """
-    Join a filename in the same directory as this function with the current working directory for the absolute path.
-    """
-    return os.path.join(cwd, filename)
-
-
-def get_request_assert(endpoint):
-    """
-    Send a GET request and assert response meets expectations.
-    """
-    r = requests.get(full_request_url(endpoint))
-    assertions(r, endpoint)
-
-
-def put_request_assert(endpoint):
-    """
-    Send a PUT request and assert response meets expectations.
-    """
-    with open(absolute_file_path('json_request_bodies.json')) as f:
-        body = json.loads(f.read())[endpoint]
-    r = requests.put(full_request_url(endpoint), data=json.dumps(body))
-    assertions(r, endpoint)
-
-
-def post_request_assert(endpoint):
-    """
-    Send a POST request and assert response meets expectations.
-    """
-    with open(absolute_file_path('json_request_bodies.json')) as f:
-        body = json.loads(f.read())[endpoint]
-    r = requests.post(full_request_url(endpoint), data=json.dumps(body),
-                      headers={"Content-Type": "application/json", "Accept": "appllication/json"})
-    assertions(r, endpoint)
-
-
-def delete_request_assert(endpoint):
-    """
-    Send a DELETE request and assert response meets expectations.
-    """
-    r = requests.delete(full_request_url(endpoint))
-    assertions(r, endpoint)
-
-
-def assertions(r, endpoint):
-    """
-    Assert response is successful and validate response body when applicable.
-    """
-    assert r.status_code is 200
-    if r.headers["Content-Type"].startswith("application/json"):
-        assert validate_schema(r.text, endpoint) is True
-    else:
-        assert False
-
-
-def validate_schema(json_str, endpoint):
-    """
-    Validate a JSON response body against a schema.
-    """
-    with open(absolute_file_path('json_schemas.json')) as f:
-        schema = json.loads(f.read())[endpoint]
-
-jsonschema.validate(json.loads(json_str), schema)
-return True
 
 
 def test_get_products_all():
 
-    get_request_assert("/products/all")
+    endpoint = "/products/all"
+    integhelpers.get_request_assert(products_api_url, endpoint, schemas_path)
 
 
 def test_get_products_id():
 
-    get_request_assert("/products/id/:product_id")
+    endpoint = "/products/id/:product_id"
+    params = {":product_id": os.getenv('TEST_PRODUCT_ID')}
+    integhelpers.get_request_assert(products_api_url, endpoint, schemas_path, params)
 
 
 def test_get_products_featured():
 
-    get_request_assert("/products/featured")
+    endpoint = "/products/featured"
+    integhelpers.get_request_assert(products_api_url, endpoint, schemas_path)
 
 
 def test_get_products_category():
 
-    get_request_assert("/products/category/:category_name")
+    endpoint = "/products/category/:category_name"
+    params = {":category_name": os.getenv('TEST_CATEGORY_ID')}
+    integhelpers.get_request_assert(products_api_url, endpoint, schemas_path, params)
 
 
 def test_put_products_id():
 
-    put_request_assert("/products/id/:product_id")
+    endpoint = "/products/id/:product_id"
+    params = {":product_id": os.getenv('TEST_PRODUCT_ID')}
+    integhelpers.put_request_assert(products_api_url, endpoint, request_bodies_path, schemas_path, params)
 
 
 # Test disabled until known issue with this endpoint is resolved
 # def test_put_products_id_inventory():
 #
-#     put_request_assert("/products/id/:product_id/inventory")
+#     endpoint = "/products/id/:product_id/inventory"
+#     params = {":product_id": os.getenv('TEST_PRODUCT_ID')}
+#     integhelpers.put_request_assert(products_api_url, endpoint, request_bodies_path, schemas_path, params)
 
 
 # Test disabled until known issue with this endpoint is resolved
 # def test_delete_products_id():
 #
-#     delete_request_assert("/products/id/:product_id")
+#     endpoint = "/products/id/:product_id"
+#     integhelpers.delete_request_assert(products_api_url, endpoint, schemas_path)
 
 
 def test_post_products():
 
-    post_request_assert("/products")
+    endpoint = "/products"
+    integhelpers.post_request_assert(products_api_url, endpoint, request_bodies_path, schemas_path)
 
 
 def test_get_categories_all():
 
-    get_request_assert("/categories/all")
+    endpoint = "/categories/all"
+    integhelpers.get_request_assert(products_api_url, endpoint, schemas_path)
 
 
 def test_get_categories_id():
 
-    get_request_assert("/categories/id/:category_id")
+    endpoint = "/categories/id/:category_id"
+    params = {":category_id": os.getenv('TEST_CATEGORY_ID')}
+    integhelpers.get_request_assert(products_api_url, endpoint, schemas_path, params)
 
