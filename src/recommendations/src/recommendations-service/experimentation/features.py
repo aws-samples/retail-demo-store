8f3d9365ee6b8080ed15d8a302f068addb0572b7
James Jory <james@jamesjory.com>
2022-04-25 17:36:04 -0700

    Add cold user feature to list of features

--- src/recommendations/src/recommendations-service/experimentation/features.py
+++ src/recommendations/src/recommendations-service/experimentation/features.py
@@ -4,6 +4,7 @@
 """ Feature names from the WebUI """
 
 FEATURE_HOME_PRODUCT_RECS = 'home_product_recs'
+FEATURE_HOME_PRODUCT_RECS_COLD = 'home_product_recs_cold'
 FEATURE_HOME_FEATURED_RERANK = 'home_featured_rerank'
 FEATURE_PRODUCT_DETAIL_RELATED = 'product_detail_related'
 FEATURE_SEARCH_RESULTS = 'search_results'
@@ -12,6 +13,7 @@ FEATURE_LIVE_DISCOUNTS = 'live_stream_prod_discounts'
 
 FEATURE_NAMES = [
     FEATURE_HOME_PRODUCT_RECS,
+    FEATURE_HOME_PRODUCT_RECS_COLD,
     FEATURE_HOME_FEATURED_RERANK,
     FEATURE_PRODUCT_DETAIL_RELATED,
     FEATURE_SEARCH_RESULTS,
