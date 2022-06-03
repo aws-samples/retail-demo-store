5e62cd0d30afc8d4edffe8560a672e1f1f450869
ijemmy <ijemmy@users.noreply.github.com>
2022-06-03 13:53:18 +0200

    Return non-zero from run-tests-integ.sh to fail the step

--- src/run-tests/run-tests-integ.sh
+++ src/run-tests/run-tests-integ.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 source .venv/bin/activate
 
 dir="$PWD"
 home_dir="$(dirname "$dir")"
-cd "$home_dir" || exit
+cd "$home_dir" || exit 1
 
 : '
 If we have accepted an argument (service name), attempt to change into that directory.
@@ -12,12 +14,36 @@ if [ ! -z "$1" ]; then
   cd "$1" || { echo "Service does not exist."; exit 1; }
 fi
 
-# Find directories containing integration tests
-find . -type d  -name 'integ' | while read dir; do
-  cd "$dir" || exit
-  find . -type f -name 'test*.py' -exec pytest {} +
-  # Change back into home directory after each set of tests have been run in order to reset process.
-  cd "$home_dir" || exit
-done
+#########################
+####### Functions #######
+#########################
+# Execute pytest in all directory with the name "integ"
+run_all_tests() {
+  failed_tests=0
+  total_tests=0
+  for dir in $(find . -type d  -name 'integ')
+  do
+    pushd "$dir"
+    pytest test*.py || failed_tests=$[failed_tests + 1]
+    total_tests=$[total_tests + 1]
+
+    # Change back into home directory after each set of tests
+    popd
+  done
+
+  echo "####################"
+  echo "Summary: ($(failed_tests)/$(total_tests)) test suits fail "
+  echo "####################"
+  return $failed_tests
+}
+
+
+run_all_tests
+result=$?
 
 deactivate
+
+exit $result
+
+
+
