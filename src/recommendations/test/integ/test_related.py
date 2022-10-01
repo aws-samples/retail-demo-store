d1484ebd26e7ed8907287b8310ebfbe7f7fbf78c
James Jory <james@jamesjory.com>
2022-10-01 12:08:33 -0700

    Fix test syntax errors

--- src/recommendations/test/integ/test_related.py
+++ src/recommendations/test/integ/test_related.py
@@ -17,12 +17,12 @@ recommendations_api_url = os.getenv("RECOMMENDATIONS_API_URL", DEFAULT_LOCAL_API
 
 DEFAULT_PARAMS = {
     ":feature": "product_detail_related",
-    # If Optimizely is configured, this field will be used to select a specific Experiment. 
+    # If Optimizely is configured, this field will be used to select a specific Experiment.
     # The default deployment doesn't have Optmizely so this value doesn't affect the result.
     ":currentItemID": "89728417-5269-403d-baa3-04b59cdffd0a",
 }
 def test_get_related_with_default_params_should_return_a_non_empty_list_of_product():
-    endpoint = "/related?userID=:userID&feature=:feature&currentItemID=:currentItemID}"
+    endpoint = "/related?userID=:userID&feature=:feature&currentItemID=:currentItemID"
     params = DEFAULT_PARAMS
     response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
     related_resp_obj = response.json()
@@ -44,15 +44,15 @@ def test_get_related_with_numResults_params_should_return_exact_number_of_result
 
 
 def test_get_related_should_return_image_filename_by_default():
-    endpoint = "/related?userID=:userID&feature=:feature&currentItemID=:currentItemID}"
+    endpoint = "/related?userID=:userID&feature=:feature&currentItemID=:currentItemID"
     params = DEFAULT_PARAMS
     response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
     recommendation_resp_obj = response.json()
     image_field = recommendation_resp_obj[0]["product"]["image"]
 
     assert_that(image_field.startswith("http://")).is_false()
-    
-    
+
+
 def test_get_related_with_fullyQualifyImageUrls_should_return_image_url():
 
     endpoint = "/recommendations?userID=:userID&feature=:feature&currentItemID&fullyQualifyImageUrls=:fullyQualifyImageUrls"
@@ -65,4 +65,3 @@ def test_get_related_with_fullyQualifyImageUrls_should_return_image_url():
     image_field = recommendation_resp_obj[0]["product"]["image"]
 
     assert_that(image_field).starts_with('http://')
-    
