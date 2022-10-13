64aaddf3b59c9239d204b51a4b3444adc29d65b2
Bastien <bastil@amazon.co.uk>
2022-10-13 16:32:40 +0100

    integration test fixes (#397)
    * Remove invalid tests
    
    * Fix test syntax errors
    
    * npm fixes
    
    Co-authored-by: James Jory <james@jamesjory.com>

--- src/recommendations/test/integ/test_popular.py
+++ src/recommendations/test/integ/test_popular.py
@@ -31,38 +31,14 @@ def test_get_popular_with_numResults_should_return_exact_number_of_result():
     num_results = 3
 
     endpoint = "/popular?userID=:userID&feature=:feature&numResults=:numResults"
-    params = { 
-        **DEFAULT_PARAMS, 
+    params = {
+        **DEFAULT_PARAMS,
         ":numResults": num_results
     }
     response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
     recommendation_resp_obj = response.json()
 
     assert_that(recommendation_resp_obj).is_length(num_results)
-
-
-def test_get_popular_with_currentItemID_should_return_different_product():
-    # Calling without "currentItemID" param should get this product id
-    endpoint = "/popular?userID=:userID&feature=:feature"
-    params = DEFAULT_PARAMS
-    response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
-
-    recommendation_resp_obj = response.json()
-    first_item_id_without_param = recommendation_resp_obj[0]["product"]["id"]
-
-    # Calling with this "currentItemID" param should get another product id
-
-    endpoint = "/popular?userID=:userID&feature=:feature&currentItemID=:currentItemID"
-    params = { 
-        **DEFAULT_PARAMS, 
-        ":currentItemID": "89728417-5269-403d-baa3-04b59cdffd0a"
-    }
-    response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
-    recommendation_resp_obj = response.json()
-    first_item_id = recommendation_resp_obj[0]["product"]["id"]
-
-    assert_that(first_item_id).is_not_equal_to(first_item_id_without_param)
-
 
 def test_get_popular_should_return_image_filename_by_default():
     endpoint = "/popular?userID=:userID&feature=:feature"
@@ -72,12 +48,12 @@ def test_get_popular_should_return_image_filename_by_default():
     recommendation_resp_obj = response.json()
     image_field = recommendation_resp_obj[0]["product"]["image"]
     assert_that(image_field.startswith("http://")).is_false()
-    
-    
+
+
 def test_get_popular_with_fullyQualifyImageUrls_should_return_image_url():
     endpoint = "/popular?userID=:userID&feature=:feature&fullyQualifyImageUrls=:fullyQualifyImageUrls"
-    params = { 
-        **DEFAULT_PARAMS, 
+    params = {
+        **DEFAULT_PARAMS,
         ":fullyQualifyImageUrls": "1"
     }
     response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
--- src/recommendations/test/integ/test_recommendations.py
+++ src/recommendations/test/integ/test_recommendations.py
@@ -21,7 +21,7 @@ DEFAULT_PARAMS = {":userID": 5097, ":feature": "product_detail_related"}
 
 def test_get_recommendations_should_return_products_with_correct_schema():
     endpoint = "/recommendations?userID=:userID&feature=:feature"
-    params = DEFAULT_PARAMS 
+    params = DEFAULT_PARAMS
     get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
 
 
@@ -38,30 +38,6 @@ def test_get_recommendations_with_numResults_should_return_exact_number_of_resul
 
     assert_that(recommendation_resp_obj).is_length(num_results)
 
-
-def test_get_recommendations_with_currentItemID_should_return_different_product():
-    # Calling without "currentItemID" param should get this product id
-    endpoint = "/recommendations?userID=:userID&feature=:feature&currentItemID"
-    params = DEFAULT_PARAMS
-    response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
-    recommendation_resp_obj = response.json()
-    first_item_id_without_param = recommendation_resp_obj[0]["product"]["id"]
-
-    # Calling with this "currentItemID" param should get another product id
-    currentItemID = "89728417-5269-403d-baa3-04b59cdffd0a"
-
-    endpoint = "/recommendations?userID=:userID&feature=:feature&currentItemID=:currentItemID"
-    params = {
-        **DEFAULT_PARAMS,
-        ":currentItemID": currentItemID
-    }
-    response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
-    recommendation_resp_obj = response.json()
-    first_item_id = recommendation_resp_obj[0]["product"]["id"]
-
-    assert_that(first_item_id).is_not_equal_to(first_item_id_without_param)
-
-
 def test_get_recommendations_should_return_image_filename_by_default():
     endpoint = "/recommendations?userID=:userID&feature=:feature&currentItemID"
     params = DEFAULT_PARAMS
@@ -70,8 +46,8 @@ def test_get_recommendations_should_return_image_filename_by_default():
     image_field = recommendation_resp_obj[0]["product"]["image"]
 
     assert_that(image_field.startswith("http://")).is_false()
-    
-    
+
+
 def test_get_recommendations_with_fullyQualifyImageUrls_should_return_image_url():
     endpoint = "/recommendations?userID=:userID&feature=:feature&currentItemID&fullyQualifyImageUrls=:fullyQualifyImageUrls"
     params = {
@@ -81,5 +57,5 @@ def test_get_recommendations_with_fullyQualifyImageUrls_should_return_image_url(
     response = get_request_assert(recommendations_api_url, endpoint, schemas_path, params)
     recommendation_resp_obj = response.json()
     image_field = recommendation_resp_obj[0]["product"]["image"]
-    
+
     assert_that(image_field).starts_with('http://')
