e531078b68a64de1c8b6f4abd249cdbdb3e2e479
Jeff Patzer <jpatzer@llnw.com>
2022-12-16 15:24:56 -0700

    update instructions and packages

--- src/web-ui/edgio/webpack.edgio.config.js
+++ src/web-ui/edgio/webpack.edgio.config.js
@@ -1,25 +1,26 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
-    browser: './edgio/browser.js',
-    'service-worker': './edgio/service-worker.js',
+    browser: "./edgio/browser.js",
+    "service-worker": "./edgio/service-worker.js",
   },
-  mode: 'production',
+  mode: "production",
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js"],
+    mainFields: ["main"],
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../dist-edgio'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../dist-edgio"),
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.DEBUG_SW': JSON.stringify(false),
+      "process.env.DEBUG_SW": JSON.stringify(false),
     }),
   ],
 };
