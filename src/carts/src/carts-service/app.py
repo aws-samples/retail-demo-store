275ba371c89f5afb23ef90d38618fd05ed8dbc4f
Benedict Nartey-Tokoli <67244216+Adibuer-lab@users.noreply.github.com>
2023-09-12 14:04:41 +0100

    explicitly add routes and error handlers to carts service using blueprints rather than relying on import side effects (#487)

--- src/carts/src/carts-service/app.py
+++ src/carts/src/carts-service/app.py
@@ -4,15 +4,16 @@
 import logging
 import os
 from server import app
+from handlers import handler_bp
+from routes import route_bp
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler()])
-
+app.register_blueprint(handler_bp)
+app.register_blueprint(route_bp)
 # Log a message at the start of the script
 app.logger.info('Starting app.py')
 
-import handlers
-import routes
 
 if __name__ == '__main__':
     try:
