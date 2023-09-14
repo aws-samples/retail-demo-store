f896a54042d512210c9159ff5b9f21920cc1e7d1
Benedict Nartey-Tokoli <benedicttokoli@gmail.com>
2023-09-14 12:59:52 +0100

    fix too long lines

--- src/orders/test/integ/test-orders.py
+++ src/orders/test/integ/test-orders.py
@@ -1,7 +1,6 @@
 import testhelpers.integ as integhelpers
 import json
 import os
-from random import randint
 import pytest
 from dotenv import load_dotenv
 import requests
@@ -55,12 +54,19 @@ def test_get_orders_id(created_order):
 def test_put_orders_id():
     endpoint = "/orders/id/:order_id"
     params = {":order_id": test_order_id}
-    integhelpers.put_request_assert(orders_api_url, endpoint, request_bodies_path, schemas_path, params)
+    integhelpers.put_request_assert(orders_api_url, 
+                                    endpoint, 
+                                    request_bodies_path, 
+                                    schemas_path, 
+                                    params)
 
 
 def test_post_orders():
     endpoint = "/orders"
-    integhelpers.post_request_assert(orders_api_url, endpoint, request_bodies_path, schemas_path)
+    integhelpers.post_request_assert(orders_api_url, 
+                                     endpoint, 
+                                     request_bodies_path, 
+                                     schemas_path)
 
 
 def test_get_orders_username():
