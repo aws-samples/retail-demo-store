36dd141201b4cfba0d1c0df8f38052953a2a41da
Benedict Nartey-Tokoli <benedicttokoli@gmail.com>
2024-06-27 15:39:02 +0100

    use batch write for loading, parallelise later

--- src/users/src/users_service/repository.py
+++ src/users/src/users_service/repository.py
@@ -25,11 +25,28 @@ def init():
 
 def load_users_into_dynamodb(filename: str) -> int:
     count = 0
+    batch_size = 25  
+    users_batch = []
+    
     with gzip.open(filename, 'rt', encoding='utf-8') as file:
         data = json.load(file)
         for user_data in data:
-            upsert_user(user_data)
-            count += 1
+            users_batch.append(user_data)
+            
+            if len(users_batch) == batch_size:
+                success, failure = batch_write_users(users_batch)
+                count += success
+                if failure > 0:
+                    current_app.logger.warning(f"{failure} users failed to write in this batch")
+                users_batch = []
+        
+        # Write any remaining users
+        if users_batch:
+            success, failure = batch_write_users(users_batch)
+            count += success
+            if failure > 0:
+                current_app.logger.warning(f"{failure} users failed to write in the final batch")
+    
     return count
 
 def claim_user(user_id: str) -> Tuple[Optional[User], str]:
@@ -49,59 +66,7 @@ def upsert_user(user_data: Dict[str, Any], user_id: Optional[str] = None, condit
     if not user_id:
         user_id = user_data.pop('id', None)
     
-    update_expression = "SET "
-    expression_attribute_values = {}
-    expression_attribute_names = {}
-
-    valid_keys = get_valid_keys(User)
-    complex_keys = {"addresses", "id", "claimed_user", "sign_up_date", "last_sign_in_date",
-                    "age_range", "primary_persona"}
-    valid_keys = valid_keys - complex_keys
-
-    for key, value in user_data.items():
-        if key in valid_keys:
-            update_expression += f"#{key} = :{key}, "
-            expression_attribute_names[f"#{key}"] = key
-            expression_attribute_values[f":{key}"] = value
-        elif key in ["sign_up_date", "last_sign_in_date"]:
-            if value:
-                parsed_date = parse_iso_datetime(value)
-            if parsed_date:
-                update_expression += f"#{key} = :{key}, "
-                expression_attribute_names[f"#{key}"] = key
-                expression_attribute_values[f":{key}"] = parsed_date.isoformat()
-            else:
-                current_app.logger.warning(f"Invalid date format for '{key}': {value}")
-        elif key not in complex_keys:
-            current_app.logger.warning(f"Attribute '{key}' not found on User model; ignoring.")
-
-    if "age" in user_data:
-        age_range = get_age_range(user_data['age'])
-        update_expression += "#age_range = :age_range, "
-        expression_attribute_names["#age_range"] = "age_range"
-        expression_attribute_values[":age_range"] = age_range
-
-    if "addresses" in user_data:
-        addresses = user_data['addresses']
-        update_expression += "#addresses = :addresses, "
-        expression_attribute_names["#addresses"] = "addresses"
-        expression_attribute_values[":addresses"] = addresses
-        
-    if "persona" in user_data:
-        primary_persona = user_data["persona"].split("_")[0]
-        update_expression += "#primary_persona = :primary_persona, "
-        expression_attribute_names["#primary_persona"] = "primary_persona"
-        expression_attribute_values[":primary_persona"] = primary_persona
-
-    if "claimed_user" in user_data:
-        update_expression += "#claimed_user = :claimed_user, "
-        expression_attribute_names["#claimed_user"] = "claimed_user"
-        expression_attribute_values[":claimed_user"] = Decimal(user_data['claimed_user'])
-    else:
-        update_expression += "claimed_user = if_not_exists(claimed_user, :claimed_user), "
-        expression_attribute_values[":claimed_user"] = Decimal(0)
-
-    update_expression = update_expression.rstrip(", ")
+    update_expression, expression_attribute_names, expression_attribute_values, _ = prepare_user_data_for_dynamodb(user_data)
 
     if update_expression != "SET ":
         try:
@@ -136,6 +101,80 @@ def upsert_user(user_data: Dict[str, Any], user_id: Optional[str] = None, condit
     else:
         current_app.logger.warning(f"No valid update actions were found for user ID {user_id}.")
         return None, "No valid update actions"
+    
+def batch_write_users(users: List[Dict[str, Any]]) -> Tuple[int, int]:
+    success_count = 0
+    failure_count = 0
+    with User.table.batch_writer() as batch:
+        for user_data in users:
+            try:
+                _, _, _, prepared_data = prepare_user_data_for_dynamodb(user_data)
+                prepared_data['id'] = user_data['id']
+                current_app.logger.debug(f"Writing user {user_data.get('id')}: {prepared_data}")
+                batch.put_item(Item=prepared_data)
+                success_count += 1
+            except ClientError as e:
+                current_app.logger.error(f"Error batch writing user {user_data.get('id')}: {str(e)}")
+                failure_count += 1
+    return success_count, failure_count
+
+def prepare_user_data_for_dynamodb(user_data: Dict[str, Any]) -> Tuple[Dict[str, Any], Dict[str, str], Dict[str, Any]]:
+    update_expression = "SET "
+    expression_attribute_values = {}
+    expression_attribute_names = {}
+    expression_attribute_data = {}
+
+    valid_keys = get_valid_keys(User)
+    complex_keys = {"addresses", "id", "claimed_user", "sign_up_date", "last_sign_in_date",
+                    "age_range", "primary_persona"}
+    valid_keys = valid_keys - complex_keys
+
+    for key, value in user_data.items():
+        if key in valid_keys:
+            update_expression += f"#{key} = :{key}, "
+            expression_attribute_names[f"#{key}"] = key
+            expression_attribute_data[key] = expression_attribute_values[f":{key}"] = value
+        elif key in ["sign_up_date", "last_sign_in_date"]:
+            if value:
+                parsed_date = parse_iso_datetime(value)
+            if parsed_date:
+                update_expression += f"#{key} = :{key}, "
+                expression_attribute_names[f"#{key}"] = key
+                expression_attribute_data[key] = expression_attribute_values[f":{key}"] = parsed_date.isoformat()
+            else:
+                current_app.logger.warning(f"Invalid date format for '{key}': {value}")
+        elif key not in complex_keys:
+            current_app.logger.warning(f"Attribute '{key}' not found on User model; ignoring.")
+
+    if "age" in user_data:
+        age_range = get_age_range(user_data['age'])
+        update_expression += "#age_range = :age_range, "
+        expression_attribute_names["#age_range"] = "age_range"
+        expression_attribute_data["age_range"] = expression_attribute_values[":age_range"] = age_range
+
+    if "addresses" in user_data:
+        addresses = user_data['addresses']
+        update_expression += "#addresses = :addresses, "
+        expression_attribute_names["#addresses"] = "addresses"
+        expression_attribute_data["addresses"] = expression_attribute_values[":addresses"] = addresses
+        
+    if "persona" in user_data:
+        primary_persona = user_data["persona"].split("_")[0]
+        update_expression += "#primary_persona = :primary_persona, "
+        expression_attribute_names["#primary_persona"] = "primary_persona"
+        expression_attribute_data["primary_persona"] = expression_attribute_values[":primary_persona"] = primary_persona
+
+    if "claimed_user" in user_data:
+        update_expression += "#claimed_user = :claimed_user, "
+        expression_attribute_names["#claimed_user"] = "claimed_user"
+        expression_attribute_data["claimed_user"] = expression_attribute_values[":claimed_user"] = Decimal(user_data['claimed_user'])
+    else:
+        update_expression += "claimed_user = if_not_exists(claimed_user, :claimed_user), "
+        expression_attribute_data["claimed_user"] = expression_attribute_values[":claimed_user"] = Decimal(0)
+
+    update_expression = update_expression.rstrip(", ")
+
+    return update_expression, expression_attribute_names, expression_attribute_values, expression_attribute_data
     
 def parse_iso_datetime(date_str: str) -> Optional[datetime]:
     try:
@@ -296,24 +335,3 @@ def update_claimed_index():
     except Exception as e:
         current_app.logger.error(f"Error in update_claimed_index: {e}")
         return {"error": str(e)}
-
-# Additional utility functions
-
-def batch_write_users(users: List[User]) -> Tuple[int, int]:
-    """
-    Batch write users to DynamoDB table.
-    Returns a tuple of (success_count, failure_count)
-    
-    next: Adjust this and upsert_user to allow dynamodb load to this
-    """
-    success_count = 0
-    failure_count = 0
-    with User.table.batch_writer() as batch:
-        for user in users:
-            try:
-                batch.put_item(Item=user.to_dict())
-                success_count += 1
-            except ClientError as e:
-                current_app.logger.error(f"Error batch writing user {user.id}: {str(e)}")
-                failure_count += 1
-    return success_count, failure_count
