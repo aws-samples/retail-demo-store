8eb8d36d76930bc72ce883da56f77329e2407172
Gareth Faires <109661363+gfaires@users.noreply.github.com>
2024-11-14 10:43:57 +0000

    Remove Evidently

--- src/recommendations/src/recommendations-service/run_tests.py
+++ src/recommendations/src/recommendations-service/run_tests.py
@@ -5,8 +5,6 @@ import os
 import sys
 import unittest
 
-os.environ['EVIDENTLY_PROJECT_NAME'] = 'retaildemostore'
-
 sys.argv += ['discover', os.path.dirname(sys.argv[0]), 'test_*.py']
 
 unittest.main(module=None)
